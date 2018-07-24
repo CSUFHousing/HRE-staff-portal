ecf4f76a8075e42438268d1e2aece104bf03f8f5
Luke Higgott <higgottluke@gmail.com>
2018-07-24 17:44:03 -0400

    naturally

--- API/slack.py
+++ API/slack.py
@@ -1,9 +1,11 @@
 from django.http.response import JsonResponse as JSR
 from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 
 from portal.models import notify_devs
 
+@csrf_exempt
 def respond(request):
     body = json.loads(request.body)
     if 'challenge' in body:
